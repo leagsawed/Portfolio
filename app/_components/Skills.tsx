eccd187a31ed822d6497f80025ae0aeeddc7653e
leagsawed <gael.dewas@gmail.com>
2024-11-22 14:26:13 +0100

    Ajout de la formation

--- app/_components/Skills.tsx
+++ app/_components/Skills.tsx
@@ -12,6 +12,23 @@ export const Skills = () => {
           Compétences
         </h2>
       </div>
+      <div className="text-center w-full">
+        <p>
+          <span className="italic text-muted-foreground">
+            Lors de la formation Développeur-web d&apos;
+          </span>
+          <a
+            href="https://openclassrooms.com/fr/"
+            className="hover:text-white/80 text-muted-foreground transition-colors duration-300"
+          >
+            <strong>OPENCLASSROOMS</strong>
+          </a>
+          <span className="italic text-muted-foreground">
+            , j&apos;ai pu apprendre à maîtriser:
+          </span>
+        </p>
+      </div>
+
       <div className="flex max-sm:flex-col gap-4">
         <Card className="flex flex-col gap-4 flex-1 items-center p-4">
           <NodeLogo size={64} className="md:w-16 sm:h-16 w-12 h-12" />
